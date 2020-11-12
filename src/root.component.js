0452af781f06966b65eb735cfc4792d2be590a4d
Shashank384 <shashank.mayur@gmail.com>
2020-11-12 15:50:53 +0530

    it is a contact microfrontend

---
+++ src/root.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./root.component.css";
+
+export default function Root() {
+  return (
+    <div className="container1">
+      <h2>Welcome to Contact Page!</h2>
+      
+    </div>
+  );
+}
